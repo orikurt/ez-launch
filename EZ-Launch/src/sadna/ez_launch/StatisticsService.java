8e8fb5fd5a787e3c7eb2543a0d64961ae0b79e2b
moramrani <moramrani@5638063c-73d5-4e75-f8bd-a2ec72647ec1>
2012-12-01 09:23:15 +0000

    Add Service Structure (StatisticsService)

---
+++ EZ-Launch/src/sadna/ez_launch/StatisticsService.java
@@ -0,0 +1,28 @@
+package sadna.ez_launch;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class StatisticsService extends Service{
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		//code to execute when the service is first created
+	}
+
+	@Override
+	public void onDestroy() {
+		//code to execute when the service is shutting down
+	}
+
+	@Override
+	public void onStart(Intent intent, int startid) {
+		//code to execute when the service is starting up
+	}	
+}
