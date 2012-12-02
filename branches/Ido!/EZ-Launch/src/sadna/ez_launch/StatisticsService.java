a16e3cb818268cee97edabfa03ba835271f86585
moramrani <moramrani@5638063c-73d5-4e75-f8bd-a2ec72647ec1>
2012-12-02 21:51:57 +0000

    GetRecentTasks Works.

--- branches/Ido!/EZ-Launch/src/sadna/ez_launch/StatisticsService.java
+++ branches/Ido!/EZ-Launch/src/sadna/ez_launch/StatisticsService.java
@@ -1,49 +1,37 @@
 package sadna.ez_launch;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import sadna.ez_launch.MainActivity.StatisticsUpdateReceiver;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RecentTaskInfo;
 import android.app.ActivityManager.RunningTaskInfo;
 import android.app.Service;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.ResolveInfo;
-import android.os.Bundle;
 import android.os.IBinder;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
-import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
 
 public class StatisticsService extends Service{
 
 	ArrayList<Shortcut> mList;
 	public static StatisticsService sInstance;
 	SystemIntentsReceiver systemIntentsReceiver;
-	
+	PackageManager pk;
+
 	private int MAX_TASKS = 10;
 	public static String UPDATE_INTENT = "sadna.ez_launch.UPDATE_INTENT";
 
 	public static StatisticsService getInstance() {
 		return sInstance;
 	}
-	
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
@@ -53,13 +41,14 @@ public class StatisticsService extends Service{
 	public void onCreate() {
 		//code to execute when the service is first created
 		sInstance = this;
-		
+
 		initData();
-		
+
 		// Register to StatisticsService.UPDATE_INTENT;
 		if (systemIntentsReceiver == null)
 			systemIntentsReceiver = new SystemIntentsReceiver();
 		registerReceiver(systemIntentsReceiver, new IntentFilter(Intent.ACTION_SCREEN_OFF));
+		//registerReceiver(systemIntentsReceiver, new IntentFilter(Intent.ACTION_USER_FOREGROUND));
 	}
 
 	@Override
@@ -75,6 +64,7 @@ public class StatisticsService extends Service{
 
 	public void initData() {
 		mList= new ArrayList<Shortcut>();
+		pk=getPackageManager();
 	}
 
 	public void sendStatistics() {		
@@ -82,46 +72,36 @@ public class StatisticsService extends Service{
 		sendBroadcast(i);
 	}
 
-//	private StringBuilder GetRunningActivityManager() {
-//		ActivityManager actvityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
-//
-//		// get the info from the currently running task
-//		List< RunningTaskInfo > tasksInfo = actvityManager.getRunningTasks(MAX_TASKS);
-//
-//		final StringBuilder logs = new StringBuilder();
-//		String separator = System.getProperty("line.separator"); 
-//
-//		for (RunningTaskInfo taskInfo : tasksInfo) {
-//
-//			ComponentName componentInfo = taskInfo.baseActivity;
-//			componentInfo.getPackageName();
-//
-//			logs.append(componentInfo);
-//			logs.append(separator);
-//		}
-//		return logs;
-//	}
+	private void GetFrontActivity() {
+		ActivityManager actvityManager = (ActivityManager) this.getBaseContext().getSystemService(ACTIVITY_SERVICE);
 
-	private void GetRecentTasks() {
-	ActivityManager actvityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
-
-	// get the info from the currently running task
-	List< RecentTaskInfo > tasksInfo = actvityManager.getRecentTasks(MAX_TASKS, ActivityManager.RECENT_IGNORE_UNAVAILABLE);
-	PackageManager pk = getPackageManager();
-
-	for (RecentTaskInfo taskInfo : tasksInfo) {
+		// get the info from the currently running task
+		List< RunningTaskInfo > tasksInfo = actvityManager.getRunningTasks(1);
 		Shortcut a;
 		try {
-			a = new Shortcut(pk.getApplicationIcon(taskInfo.origActivity.getPackageName()),
-					taskInfo.origActivity.getPackageName());
+			a = new Shortcut(pk.getApplicationIcon(tasksInfo.get(0).baseActivity.getPackageName()),
+					tasksInfo.get(0).baseActivity.getPackageName());
 			mList.add(a);
 		} catch (NameNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-}
-	
+
+	private void GetRecentTasks() {
+		ActivityManager actvityManager = (ActivityManager) this.getBaseContext().getSystemService(ACTIVITY_SERVICE);
+
+		List< RecentTaskInfo > tasksInfo = actvityManager.getRecentTasks(MAX_TASKS, ActivityManager.RECENT_IGNORE_UNAVAILABLE);
+
+		for (RecentTaskInfo taskInfo : tasksInfo) {
+			Shortcut a;
+			ResolveInfo resolveInfo = pk.resolveActivity(taskInfo.baseIntent, PackageManager.MATCH_DEFAULT_ONLY);
+			String packageName = resolveInfo.activityInfo.packageName;
+			a = new Shortcut(resolveInfo.activityInfo.loadIcon(getPackageManager()), packageName);
+			mList.add(a);
+		}
+	}
+
 	private void GetInstalledApplicationsList()
 	{
 		mList.clear();
@@ -131,23 +111,22 @@ public class StatisticsService extends Service{
 		final Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
 		mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
 		List<ResolveInfo> pkgAppsList = pk.queryIntentActivities( mainIntent, 0);
-		
+
 		List<Scoring> scoringList = new ArrayList<Scoring>();
 		for (ResolveInfo resolveInfo : pkgAppsList) {
 			if (resolveInfo.loadLabel(pk).toString().equalsIgnoreCase("calculator")){
 				scoringList.add(new Scoring(200, resolveInfo));
 			}
-			else{if (resolveInfo.loadLabel(pk).toString().equalsIgnoreCase("gallery")){
+			else if (resolveInfo.loadLabel(pk).toString().equalsIgnoreCase("gallery")){
 				scoringList.add(new Scoring(300, resolveInfo));
 			}
-			else{
+			else {
 				scoringList.add(new Scoring(0, resolveInfo));
 			}
-			}
 		}
-		
+
 		Collections.sort(scoringList, new ScoringComp());
-		
+
 		for (Scoring s : scoringList) {
 
 			Shortcut a = new Shortcut((s.info.activityInfo.loadIcon(getPackageManager())),
@@ -156,36 +135,41 @@ public class StatisticsService extends Service{
 				mList.add(a);
 		}
 	}
-	
+
 	public class SystemIntentsReceiver extends BroadcastReceiver {
 		@Override
 		public void onReceive(Context context, Intent intent) {
-			
+
 			if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
-				GetInstalledApplicationsList();
-				//GetRecentTasks();
-				
+				//GetInstalledApplicationsList();
+				//GetFrontActivity();
+				GetRecentTasks();
 				sendStatistics();
 			}
+
+			//			if (intent.getAction().equals(Intent.ACTION_USER_FOREGROUND)) {
+			//				GetFrontActivity();
+			//				sendStatistics();
+			//			}
 		}
 	}
-	
+
 	public class ScoringComp implements Comparator<Scoring>{
-		
+
 		@Override
 		public int compare(Scoring s1, Scoring s2){
-			return (s1.score>s2.score ? 1 : (s1.score==s2.score ? 0 : -1));
+			return (s1.score>s2.score ? -1 : (s1.score==s2.score ? 0 : 1));
 		}
 	}
-	
+
 	public class Scoring{
 		int score;
 		ResolveInfo info;
-		
+
 		public Scoring(int s, ResolveInfo i){
 			this.score = s;
 			this.info = i;
 		}
-	
+
 	}
 }
